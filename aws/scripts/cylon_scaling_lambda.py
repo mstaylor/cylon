eb32e195048ed415e42c9785f0663502f155edd3
APKAI5AM767AKRTSIELQ <bud.staylor@gmail.com>
2024-03-30 14:46:59 -0400

    partial - lambda updates based on rendezvous and tcpunch integration

--- aws/scripts/cylon_scaling_lambda.py
+++ aws/scripts/cylon_scaling_lambda.py
@@ -67,9 +67,9 @@ def cylon_join(data=None, ipAddress = None):
 
     #os.environ['UCX_TCP_CONN_NB'] = "y" #set to noblocking
     #os.environ['UCX_TCP_ENABLE_REDIS'] = "y" #enable redis for lambda hole punch
-    #os.environ['UCX_TCP_ENABLE_TCPUNCH'] = "y" #enable holepunching via ucx
-    #os.environ['UCX_TCP_REDIS_IP'] = data['redis_host']
-    #os.environ['UCX_TCP_REDIS_PORT'] = f"{data['redis_port']}"
+    os.environ['UCX_TCP_ENABLE_NAT_TRAVERSAL'] = "y" #enable holepunching via ucx
+    os.environ['UCX_TCP_REDIS_IP'] = data['redis_host']
+    os.environ['UCX_TCP_REDIS_PORT'] = f"{data['redis_port']}"
     #os.environ['UCX_TCP_REUSE_SOCK_ADDR'] = '1'
 
     if ipAddress is not None:
@@ -77,6 +77,7 @@ def cylon_join(data=None, ipAddress = None):
         os.environ['UCX_TCP_REMOTE_ADDRESS_OVERRIDE'] = ipAddress
 
     os.environ['UCX_TCP_IGNORE_IFNAME'] = 'y'
+
 
 
     redis_context = UCCRedisOOBContext(data['world_size'], f"tcp://{data['redis_host']}:{data['redis_port']}")
@@ -97,6 +98,7 @@ def cylon_join(data=None, ipAddress = None):
     print("retrieved cylon env")
 
     context = env.context
+
 
     if context is None:
         print("unable to retrieve cylon context")
@@ -390,8 +392,6 @@ if __name__ == "__main__":
                         **environ_or_required('S3_SUMMARY_OBJECT_NAME'))
 
     args = vars(parser.parse_args())
-
-
     os.environ['EXPOSE_ENV'] = "1-65535"
     os.environ['UCX_LOG_LEVEL'] = "TRACE"
     os.environ['UCX_LOG_LEVEL_TRIGGER'] = "TRACE"
