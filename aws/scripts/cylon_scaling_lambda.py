43ba9c634aacc609a943a09a36502bc278eb0da0
APKAI5AM767AKRTSIELQ <bud.staylor@gmail.com>
2024-05-13 18:20:27 -0400

    updates dockerfile to remove natcheck from image

--- aws/scripts/cylon_scaling_lambda.py
+++ aws/scripts/cylon_scaling_lambda.py
@@ -447,7 +447,7 @@ if __name__ == "__main__":
 
         if args['operation'] == 'join':
             print("executing cylon join operation")
-            cylon_join(args, private_ip)
+            cylon_join(args, None)
         elif args['operation'] == 'sort':
             print("executing cylon sort operation")
             cylon_sort(args)
