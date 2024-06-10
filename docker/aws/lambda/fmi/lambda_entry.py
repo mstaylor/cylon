60d6a84e56a25bd259470e56b6aeaf0d49eaeb5b
APKAI5AM767AKRTSIELQ <bud.staylor@gmail.com>
2024-06-10 12:34:26 -0400

    updates to support redis logging

---
+++ docker/aws/lambda/fmi/lambda_entry.py
@@ -0,0 +1,15 @@
+import sys
+import time
+import argparse
+import subprocess
+
+import boto3
+from botocore.exceptions import ClientError
+import os
+
+import logging
+
+
+
+def handler(event, context):
+    print("running")
