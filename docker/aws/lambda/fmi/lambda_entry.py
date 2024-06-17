f73797a4adf849dd21e263e442d12c0e8af694fa
mstaylor <bud.staylor@gmail.com>
2024-06-17 13:24:16 -0400

    updates fmi dockerfile

--- docker/aws/lambda/fmi/lambda_entry.py
+++ docker/aws/lambda/fmi/lambda_entry.py
@@ -11,6 +11,36 @@ from botocore.exceptions import ClientError
 
 import time
 import argparse
+import logging
+import os
+
+def environ_or_required(key):
+    return (
+        {'default': os.environ.get(key)} if os.environ.get(key)
+        else {'required': True}
+    )
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
 
 def fmi_send_receive(data=None):
 
