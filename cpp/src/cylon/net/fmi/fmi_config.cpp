095b9fc6410a8d7e0f08323de878497b52855e18
mstaylor <bud.staylor@gmail.com>
2024-08-15 16:06:03 -0400

    updates fmi dockerfile

--- cpp/src/cylon/net/fmi/fmi_config.cpp
+++ cpp/src/cylon/net/fmi/fmi_config.cpp
@@ -1,3 +1,11 @@
 //
 // Created by parallels on 7/2/24.
 //
+
+#include <cylon/net/fmi/fmi_config.hpp>
+
+namespace cylon {
+    namespace net {
+
+    }
+    }
--- cpp/src/cylon/net/fmi/fmi_config.hpp
+++ cpp/src/cylon/net/fmi/fmi_config.hpp
@@ -5,4 +5,25 @@
 #ifndef CYLON_FMI_CONFIG_HPP
 #define CYLON_FMI_CONFIG_HPP
 
+#include <string>
+
+namespace cylon {
+    namespace net {
+        class FMIOptions {
+        private:
+            std::string rendezvous_host = "cylon-rendezvous.aws-cylondata.com";
+
+        public:
+            FMIOptions();
+
+            /**
+            * Change the default delimiter(",")
+            * @param delimiter character representing the delimiter
+            */
+            FMIOptions WithRendezvousHost(char delimiter);
+        };
+
+    }
+
+}
 #endif //CYLON_FMI_CONFIG_HPP
