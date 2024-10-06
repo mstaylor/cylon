d2c2226c1be1bf2432d419b15a012bd70853bad2
APKAI5AM767AKRTSIELQ <bud.staylor@gmail.com>
2024-10-06 13:05:22 -0400

    opentelemetry integration

--- target/aws/scripts/ecs/runCylonWithOpenTelemetry.sh
+++ target/aws/scripts/ecs/runCylonWithOpenTelemetry.sh
@@ -12,5 +12,7 @@ conda activate cylon_dev
 set -euo pipefail
 
 export LD_LIBRARY_PATH=/cylon/install/lib
+export OTEL_EXPORTER_OTLP_ENDPOINT=http://cylon-collector1.aws-cylondata.com:4318
+export OTEL_EXPORTER_OTLP_PROTOCOL=grpc
 
 exec opentelemetry-instrument python /cylon/target/aws/scripts/ecs/S3_run_script.py
