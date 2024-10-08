5cb4c0bce5fbe3af4c237bfa9278ea5bd5e43147
APKAI5AM767AKRTSIELQ <bud.staylor@gmail.com>
2024-10-08 17:37:34 -0400

    opentelemetry integration - revert to grafanna

--- target/aws/scripts/ecs/runCylonWithOpenTelemetry.sh
+++ target/aws/scripts/ecs/runCylonWithOpenTelemetry.sh
@@ -15,25 +15,25 @@ export LD_LIBRARY_PATH=/cylon/install/lib
 
 # Uncomment the appropriate protocol for your programming language.
 # Only for OTLP/gRPC.
-export OTEL_EXPORTER_OTLP_ENDPOINT="https://otlp.uptrace.dev:4317"
+#export OTEL_EXPORTER_OTLP_ENDPOINT="https://otlp.uptrace.dev:4317"
 # Only for OTLP/HTTP.
 #export OTEL_EXPORTER_OTLP_ENDPOINT="https://otlp.uptrace.dev"
 
 # Pass Uptrace DSN in gRPC/HTTP headers.
-export OTEL_EXPORTER_OTLP_HEADERS="uptrace-dsn=https://8FpWBspz0xRNjOGok5vKGA@api.uptrace.dev?grpc=4317"
+#export OTEL_EXPORTER_OTLP_HEADERS="uptrace-dsn=https://8FpWBspz0xRNjOGok5vKGA@api.uptrace.dev?grpc=4317"
 
 # Enable gzip compression.
-export OTEL_EXPORTER_OTLP_COMPRESSION=gzip
+#export OTEL_EXPORTER_OTLP_COMPRESSION=gzip
 
 # Enable exponential histograms.
-export OTEL_EXPORTER_OTLP_METRICS_DEFAULT_HISTOGRAM_AGGREGATION=BASE2_EXPONENTIAL_BUCKET_HISTOGRAM
+#export OTEL_EXPORTER_OTLP_METRICS_DEFAULT_HISTOGRAM_AGGREGATION=BASE2_EXPONENTIAL_BUCKET_HISTOGRAM
 
 # Prefer delta temporality.
-export OTEL_EXPORTER_OTLP_METRICS_TEMPORALITY_PREFERENCE=DELTA
+#export OTEL_EXPORTER_OTLP_METRICS_TEMPORALITY_PREFERENCE=DELTA
 
 
-#export OTEL_EXPORTER_OTLP_ENDPOINT=http://cylon-collector1.aws-cylondata.com:4317
-#export OTEL_EXPORTER_OTLP_PROTOCOL=grpc
+export OTEL_EXPORTER_OTLP_ENDPOINT=http://cylon-collector1.aws-cylondata.com:4317
+export OTEL_EXPORTER_OTLP_PROTOCOL=grpc
 export OTEL_SERVICE_NAME=cylon
 
 exec opentelemetry-instrument --logs_exporter otlp python /cylon/target/aws/scripts/ecs/S3_run_script.py
