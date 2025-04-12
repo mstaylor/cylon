56bca5fa685ee5e3448dc70c131dabbc0c442af4
mstaylor <bud.staylor@gmail.com>
2025-04-12 16:43:21 -0400

    FMI integration - partial (2 node example success!)

--- cpp/src/cylon/thridparty/fmi/Communicator.hpp
+++ cpp/src/cylon/thridparty/fmi/Communicator.hpp
@@ -120,8 +120,8 @@ namespace FMI {
         void recv(Comm::Data<T> &buf, FMI::Utils::peer_num src,
                   FMI::Utils::fmiContext * context,
                   std::function<void(FMI::Utils::NbxStatus, const std::string&, FMI::Utils::fmiContext *)> callback) {
-            channel_data data {buf.data(), buf.size_in_bytes()};
-            channel->recv(data, src, context, callback);
+            channel_data data {buf.data(), buf.size_in_bytes(), FMI::Comm::noop_deleter};
+            channel->recv(data, src, context, std::move(callback));
         }
 
         //! Broadcast the data that is in the provided buf of the root peer. Result is stored in buf for all peers.
