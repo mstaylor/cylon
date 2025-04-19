d70b58203d165d17d232bd0d07c484f6111e4c05
APKAI5AM767AKRTSIELQ <bud.staylor@gmail.com>
2025-04-19 11:40:17 -0400

    adds updated docker for cylon fmi experiments

--- target/shared/scripts/scaling/scaling.py
+++ target/shared/scripts/scaling/scaling.py
@@ -431,6 +431,10 @@ if __name__ == "__main__":
         import fmi
         from fmilib.fmi_operations import fmi_communicator
     elif args["env"] == "fmi-cylon":
+        from pycylon.frame import CylonEnv, DataFrame
+        from pycylon.net.ucc_config import UCCConfig
+        from pycylon.net.redis_ucc_oob_context import UCCRedisOOBContext
+        from pycylon.net.reduce_op import ReduceOp
         from cylonfmilib.cylon_fmi import cylon_communicator
     else:
         from pycylon.frame import CylonEnv, DataFrame
