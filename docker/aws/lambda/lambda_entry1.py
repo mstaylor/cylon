f39f725105614e8de9fc0e4270e6342ff4d34477
mstaylor <bud.staylor@gmail.com>
2026-02-12 15:24:44 -0500

    redis/s3 fixes for fmi communicator

--- docker/aws/lambda/lambda_entry1.py
+++ docker/aws/lambda/lambda_entry1.py
@@ -160,8 +160,10 @@ def handler(event, context):
         os.environ["ENABLE_FMI_PING"] = str(event["ENABLE_FMI_PING"])
     if "FMI_CHANNEL_TYPE" in event and event["FMI_CHANNEL_TYPE"] is not None:
         os.environ["FMI_CHANNEL_TYPE"] = event["FMI_CHANNEL_TYPE"]
-    if "s" in event and event["FMI_S3_REGION"] is not None:
+    if "FMI_S3_REGION" in event and event["FMI_S3_REGION"] is not None:
         os.environ["FMI_S3_REGION"] = event["FMI_S3_REGION"]
+    if "KEY_TTL" in event and event["KEY_TTL"] is not None:
+        os.environ["KEY_TTL"] = str(event["KEY_TTL"])
 
     parser = argparse.ArgumentParser(description="run S3 script")
 
