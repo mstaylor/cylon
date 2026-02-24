adeb8c6bd19db9f22a2e612ad2ba0115f916b66f
mstaylor <bud.staylor@gmail.com>
2026-02-24 17:33:25 -0500

    updates groupby experiment to use combiner

--- target/shared/scripts/scaling/scaling.py
+++ target/shared/scripts/scaling/scaling.py
@@ -925,7 +925,7 @@ if __name__ == "__main__":
     if args['operation'] == 'join':
         print("executing join operation")
         join(args, ipaddress)
-    elif args['operation'] == 'groupby':ok
+    elif args['operation'] == 'groupby':
         print("executing groupby operation")
         groupby_agg(args, ipaddress)
     elif args['operation'] == 'microbenchmark':
